f73868d5f8dac869abd26b943b287d82f7d3e0a7
CONTIGEN <69436315+CONTIGEN@users.noreply.github.com>
2021-05-22 19:25:16 +0100

    New file

---
+++ script.js
@@ -0,0 +1,11 @@
+`use strict`;
+
+// method from dycalendar script
+dycalendar.draw({
+  target: `#myCalendar`,
+  type: `month`,
+  dayformat: `full`,
+  monthformat: `full`,
+  highlighttargetdate: true,
+  prevnextbutton: `show`,
+});
